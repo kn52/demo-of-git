2fe48ebeb2aa03956665abb106e7811bd40a7e53
kn52 <ashish52922@gmail.com>
2020-07-13 22:44:35 +0530

    Added:Program to print table of a specific number

---
+++ Hello.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Hello {
+	public static void main(String[] args){
+		int number=0;
+		Scanner scan=new Scanner(System.in);
+		System.out.println("Hello to the Daemons World");
+		System.out.print("Enter number to print table: ");
+		System.out.println();
+		for(int i=0;i<=10;i++){
+			System.out.println(number+" * "+i+" = "+(number*i));
+		}
+	}
+}
+
