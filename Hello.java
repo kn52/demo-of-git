d68a4cbb0a3e42ced77281f415f48d1359ff4d5f
kn52 <ashish52922@gmail.com>
2020-07-14 18:19:22 +0530

    Added:Print odd numbers

--- Hello.java
+++ Hello.java
@@ -8,7 +8,7 @@ public class Hello {
 		System.out.print("Enter number to print table: ");
 		System.out.println();
 		for(int i=0;i<=10;i++){
-			if(i%2==0){
+			if(i%2!=0){
 				System.out.println(number+" * "+i+" = "+(number*i));
 			}
 		}
